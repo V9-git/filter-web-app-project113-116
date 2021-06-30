eb9028eeb6c3470906bcd00e43f5972c352a45f2
V9-git <77952256+V9-git@users.noreply.github.com>
2021-06-30 13:41:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+nosex=0;
+nosey=0;
+
 function preload()
 {
-
+clown_nose = loadImage('https://i.postimg.cc/3x3QzSGq/m.png')
 }
 function setup()
 {
@@ -16,6 +19,10 @@ function setup()
 function draw()
 {
     image(video, 0, 0, 300, 300);
+    fill(255,0,0);
+    stroke(255,0,0);
+    circle(nosex, nosey, 20);
+    image(clown_nose, nosex, nosey, 30, 30);
 }
 function take_snapshot()
 {
